c5402fbd7baa8028ac09cba83cc8068c4b26ff72
michael mapesa <michaelmapesa93@gmail.com>
2021-12-09 07:31:47 +0300

    add files

---
+++ script.js
@@ -0,0 +1,6 @@
+var add = function(number1, number2) {
+    return number1 + number2;
+  };
+  var number1 = prompt("Enter a number:");
+  var number2 = prompt("Enter a number:");
+  alert(parseInt(number1)+parseInt(number2));
